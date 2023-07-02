9a4ab24744448746bc828537a4efa566f807e7da
Satara <tatenokay@gmail.com>
2023-07-01 23:13:27 -0500

    Objetos

--- Objectos/test.js
+++ Objectos/test.js
@@ -27,3 +27,28 @@ const person = {
   }
 }
 
+/// ---------------- reto Midu-------///
+//Tenemos una función que recibe dos parámetros. name y subs. Haz que la función devuelva 
+//un objeto con la siguiente información:
+// name con el valor del parámetro name
+// subscribers con el valor del parámetro subs
+// hash, con el valor de la longitud del string name multiplicado por el parámetro subs
+// Un método getStatus que devuelva el texto El canal de <name> tiene <subs> suscriptores. 
+// Por ejemplo, para name = 'Dani' y subs = 100, el método getStatus devolvería 
+// El canal de Dani tiene 100 suscriptores.
+//¡Ojo! El método getStatus debe devolver el texto, NO imprimirlo por consola.
+
+function createObject(name, subs) {
+  let hash = name.length * subs
+  console.log(hash)
+  return{
+    name: name,
+    subscribers: subs,
+    hash: hash,
+    getStatus: function(){
+      return `El canal de ${this.name} tiene ${this.subscribers} suscriptores`
+    }
+  }
+}
+createObject("Miguel", 100)
+
