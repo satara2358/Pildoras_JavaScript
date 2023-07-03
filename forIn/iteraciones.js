78ca2bf9f82026bb7e4af4caabc1472c5548cf00
Satara <tatenokay@gmail.com>
2023-07-03 00:25:10 -0500

    RetoObjetos

--- forIn/iteraciones.js
+++ forIn/iteraciones.js
@@ -1,5 +1,5 @@
 // for...in nos permite crear un bucle que itera sobre todas las propiedades enumerables 
-//de un objeto, en un orden arbitrario.
+// de un objeto, en un orden arbitrario.
 
 const spiderman = {
   name: 'Spidey',
@@ -14,11 +14,13 @@ for (const property in spiderman) {
 // -> name: Spidey
 // -> universe: 42
 // -> powers: web,invisibility,spider-sense
+
 // for...in con el de for...of que vimos en la sección de arrays. El primero itera sobre 
 //las propiedades enumerables de un objeto, mientras que el segundo itera sobre los elementos 
 //de un objeto iterable (como los elementos de un array, por ejemplo).
 
-// Iterar con Object.keys()
+// -----------------Iterar con Object.keys()---------------/// 
+
 const properties = Object.keys(spiderman)
 
 console.log(properties.length) // 3
@@ -31,7 +33,7 @@ properties.forEach(property => {
 // -> universe
 // -> powers
 
-//  Object.values() ---------------///
+//  -------------- Object.values() ---------------///
 
 const values = Object.values(spiderman)
 
@@ -45,3 +47,50 @@ values.forEach(value => {
 // -> [ 'web', 'invisibility', 'spider-sense' ]
 
 // -------------- Object.entries()-------------------//
+
+const entriess = Object.entries(spiderman)
+
+entriess.forEach(entry => {
+  console.log(entry)
+})
+
+// -> [ 'name', 'Spidey' ]
+// -> [ 'universe', 42 ]
+// -> [ 'powers', [ 'web', 'invisibility', 'spider-sense' ] ]
+
+// Lo interesante es que este método nos da acceso tanto a las propiedades como a los valores. 
+// Lo puedes ver más claro en el siguiente ejemplo:
+
+const entriesss = Object.entries(spiderman)
+
+entriesss.forEach(entry => {
+  const property = entry[0]
+  const value = entry[1]
+
+  console.log(`${property}: ${value}`)
+})
+
+// name: Spidey
+// universe: 42
+// powers: web,invisibility,spider-sense
+
+/// -------------reto------------------ ///
+
+// Tienes una función que recibe un objeto como parámetro. La función debe retornar un array 
+// con el nombre de las propiedades que su tipo sea boolean.
+
+// Por ejemplo, para el objeto { a: true, b: 42, c: false } la función debe retornar 
+// ['a', 'c'] ya que son las dos propiedades que tienen valores booleanos.
+
+function getKeysOfBooleanValues(obj) {
+  const boleans = []
+
+  for (const property in obj){
+    if(typeof obj[property] === 'boolean'){
+      boleans.push(property)
+    }
+  }
+  return boleans
+}
+
+getKeysOfBooleanValues({ a: true, b: true, c: false })
